9ddfe557bdd15a9eaeb5d29ea729d666c8b69e36
promise955 <67298580+promise955@users.noreply.github.com>
2024-07-08 10:02:52 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-4 px-8 bg-gray-900 text-white text-center">
-      <p>&copy; 2024 Promise. All rights reserved.</p>
+      <p>&copy; 2024 Expense Tracker. All rights reserved.</p>
     </footer>
   );
 };
